ec3080b6027583d7c2a57b0f87530799f9c84cbf
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2024-01-25 23:40:31 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from './components/head/head.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeadComponent,MainComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [],
-=======
-  content: ["./src/**/*.{html,ts}"],
->>>>>>> f44ff8c57a4c53e9037aa6d0d0ac060eccdb77ed
+  content: ["./src/**/*.{html,ts}",],
   theme: {
     extend: {},
   },
